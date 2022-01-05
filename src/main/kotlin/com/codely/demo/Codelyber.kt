7910e668d0be15da12995ceefda31b2a5ce473cb
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2022-01-05 17:46:48 +0100

    Use scope functions 2

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -2,11 +2,26 @@ package com.codely.demo
 
 import java.time.LocalDate
 import java.time.Period
+import kotlin.system.exitProcess
 
 fun main() {
     println("Please, enter your birthday with the format <yyyy-MM-dd>:")
-    val input = readLine()
-    val birthday = LocalDate.parse(input)
-    val difference = Period.between(birthday, LocalDate.now())
-    println("Years from birthday: ${difference.years}")
+
+    readLine().takeUnless {
+        it.isNullOrEmpty() || it.isBlank()
+    }?.let {
+        LocalDate.parse(it)
+    }.apply {
+        if (this == null) {
+            println("The date is not valid")
+            exitProcess(1)
+        }
+    }?.also {
+        println("Your birthday is $it")
+    }.run {
+        with(Period.between(this, LocalDate.now())) {
+            println("Years from birthday: ${this.years}")
+        }
+    }
 }
+
